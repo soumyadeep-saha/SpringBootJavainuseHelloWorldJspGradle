2c4def95117ef031b0d2db48aca62f8499f7f25f
Soumyadeep Saha <SOUMYADEEP_SAHA@CRGL-THIRDPARTY.COM>
2018-12-25 01:27:38 +0530

    first commit

---
+++ src/main/java/com/soumyadeep/springboot/SpringBootJavainuseHelloWorldJspGradleApplication.java
@@ -0,0 +1,14 @@
+package com.soumyadeep.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavainuseHelloWorldJspGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJavainuseHelloWorldJspGradleApplication.class, args);
+	}
+
+}
+
